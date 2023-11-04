d114909becec71272c0b198834a2e361b96f5df0
LVR1605 <aubreyamante4122@gmail.com>
2023-11-04 16:08:59 +0800

    another commit

--- controller/deleteUser.php
+++ controller/deleteUser.php
@@ -1,27 +1,17 @@
 <?php
-if (isset($_GET['user_id'])) {
-    $user_id = $_GET['user_id'];
+require $_SERVER["DOCUMENT_ROOT"] . '/simpleAdmin/config/dbconnect.php';
+
+if (isset($_POST['delete_user_id'])) {
+    $user_id_to_delete = $_POST['delete_user_id'];
     
-    include "../config/db.php"; // Include your database connection code
-    
-    // First, delete user's data from the result table
-    $deleteResultQuery = "DELETE FROM result WHERE user_id = $user_id";
-    if (mysqli_query($conn, $deleteResultQuery)) {
-        // Now, delete the user from the users table
-        $deleteUserQuery = "DELETE FROM users WHERE user_id = $user_id";
-        if (mysqli_query($conn, $deleteUserQuery)) {
-            // User and related data deleted successfully
-            header("Location: viewUsers.php");
-        } else {
-            // Handle user table deletion error
-            echo "Error deleting user: " . mysqli_error($conn);
-        }
+    // Delete the user from the users table
+    $delete_sql = "DELETE FROM users WHERE user_id = $user_id_to_delete";
+    if ($conn->query($delete_sql) === TRUE) {
+        // Delete the matching user_id in the results table
+        $delete_results_sql = "DELETE FROM results WHERE user_id = $user_id_to_delete";
+        $conn->query($delete_results_sql);
     } else {
-        // Handle result table deletion error
-        echo "Error deleting user's data from the result table: " . mysqli_error($conn);
+        echo "Error deleting user: " . $conn->error;
     }
-} else {
-    // Handle missing user_id parameter
-    echo "Invalid request.";
 }
 ?>
--- controller/users.php
+++ controller/users.php
@@ -2,19 +2,8 @@
 
 require $_SERVER["DOCUMENT_ROOT"] . '/simpleAdmin/config/dbconnect.php';
 
-if (isset($_POST['delete_user_id'])) {
-    $user_id_to_delete = $_POST['delete_user_id'];
-    
-    // Delete the user from the users table
-    $delete_sql = "DELETE FROM users WHERE user_id = $user_id_to_delete";
-    if ($conn->query($delete_sql) === TRUE) {
-        // Delete the matching user_id in the results table
-        $delete_results_sql = "DELETE FROM results WHERE user_id = $user_id_to_delete";
-        $conn->query($delete_results_sql);
-    } else {
-        echo "Error deleting user: " . $conn->error;
-    }
-}
+include "deleteUser.php";
+        
 
 $sql = "SELECT * FROM users WHERE deleted_at IS null";
 $result = $conn->query($sql);
